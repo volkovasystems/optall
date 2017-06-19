940df4c2981ac0f490075a80df943ed31f64857d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-19 13:30:26 +0800

    Updated dependency.

--- optall.js
+++ optall.js
@@ -32,12 +32,10 @@
 			"file": "optall.js",
 			"module": "optall",
 			"author": "Richeve S. Bebedor",
-			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
 			],
 			"repository": "https://github.com/volkovasystems/optall.git",
 			"test": "optall-test.js",
