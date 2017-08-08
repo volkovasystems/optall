7d9215e18fdd35210ece6349cb7ef9870edb3072
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 18:26:25 +0800

    Updated test.

--- optall-test.js
+++ optall-test.js
@@ -2,6 +2,6 @@
 const assert = require( "assert" );
 const optall = require( "./optall.js" );
 
-assert.deepEqual( optall( [ 1, 2, 3 ], 2, true ), [ 2 ], "should be equal" );
+assert.deepEqual( optall( [ 1, 2, 3 ], 2, true ), [ 2 ], "should be equal to [ 2 ]" );
 
 console.log( "ok" );
